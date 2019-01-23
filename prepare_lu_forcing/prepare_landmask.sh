0fd88a403ec372cacfb4e5010431b5b4fcbba955
stineb <benjamin.stocker@gmail.com>
2019-01-23 09:35:31 +0100

    just commenting out/in

--- prepare_lu_forcing/prepare_landmask.sh
+++ prepare_lu_forcing/prepare_landmask.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 
-# Download ICE-5G files
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_21.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_20.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_19.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_18.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_17.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_16.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_16.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_15.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_15.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_14.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_14.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_13.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_13.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_12.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_12.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_11.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_11.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_10.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_10.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_09.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_09.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_08.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_08.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_07.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_07.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_06.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_06.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_05.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_05.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_04.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_04.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_03.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_03.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_02.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_02.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_01.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_01.0k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_00.5k_1deg.nc.gz"
-wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_00.0k_1deg.nc.gz"
+# # Download ICE-5G files
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_21.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_20.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_19.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_18.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_17.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_16.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_16.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_15.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_15.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_14.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_14.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_13.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_13.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_12.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_12.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_11.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_11.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_10.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_10.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_09.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_09.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_08.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_08.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_07.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_07.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_06.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_06.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_05.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_05.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_04.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_04.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_03.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_03.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_02.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_02.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_01.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_01.0k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_00.5k_1deg.nc.gz"
+# wget --header="Host: pmip2.lsce.ipsl.fr" --header="Referer: https://pmip2.lsce.ipsl.fr/design/ice5g/" "https://pmip2.lsce.ipsl.fr/share/design/ice5g/ice5g_v1.2_00.0k_1deg.nc.gz"
 
-## unzip files
-gunzip ice5g*.gz
+# ## unzip files
+# gunzip ice5g*.gz
 
 get_fland(){
 
@@ -69,6 +69,29 @@ get_fland(){
 	ncatted -a units,fland,m,c,"fraction" fland_ice5g_v1.2_$1k_1deg.nc
 
 }
+
+# test
+## take pixels with orog>0 as land. Puts 1 in all land cells and NA otherwise.
+cdo selname,orog -gtc,0 ice5g_v1.2_20.0k_1deg.nc notocean.nc
+
+## get fraction of pixel covered by ice (in percent). This is actually just a 100% or 0% field. No actual fractions are given.
+cdo selname,sftgif ice5g_v1.2_20.0k_1deg.nc isice.nc
+
+## land pixels are now where orog (notocean.nc) is 1 and where sftgif (isice.nc) is not 100. Writes 0 into pixels covered by ice, and 1 everywhere else.
+cdo nec,100 -setmisstoc,1 isice.nc fland_notice.nc
+
+## multiply fland_notice with notocean
+cdo mul -setname,fland fland_notice.nc notocean.nc fland_ice5g_v1.2_20.0k_1deg.nc
+
+## remove temporary files
+rm notocean.nc isice.nc fland_notice.nc
+
+## change variable attributes
+ncatted -a long_name,fland,m,c,"land fraction of gridcell" fland_ice5g_v1.2_20.0k_1deg.nc
+ncatted -a standard_name,fland,m,c,"fland" fland_ice5g_v1.2_20.0k_1deg.nc
+ncatted -a units,fland,m,c,"fraction" fland_ice5g_v1.2_20.0k_1deg.nc
+
+
 
 # invoke the function
 get_fland 21.0
@@ -111,10 +134,10 @@ get_fland 01.0
 get_fland 00.5
 get_fland 00.0
 
-## move to sub-directory
-mkdir -p data/ice5g/orig
-mv fland_ice5g_v1.2_*_1deg.nc data/ice5g
-mv ice5g_v1.2_*k_1deg.nc data/ice5g/orig
+# ## move to sub-directory
+# mkdir -p data/ice5g/orig
+# mv fland_ice5g_v1.2_*_1deg.nc data/ice5g
+# mv ice5g_v1.2_*k_1deg.nc data/ice5g/orig
 
 
 
